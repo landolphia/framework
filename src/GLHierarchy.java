33c9edb49db1c59b22b156aa5815a2a46db99fb4
Cyrille Dessigny <grimskunk@gmail.com>
2015-01-15 19:32:11 -0500

    [BROKEN] MatrixStack & Hierarchy placeholders

---
+++ src/GLHierarchy.java
@@ -0,0 +1,262 @@
+package framework;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+import org.lwjgl.util.vector.Vector3f;
+
+//TODO update when new version rolls out
+import org.lwjgl.util.vector.Matrix4f;
+
+import java.nio.FloatBuffer;
+import org.lwjgl.BufferUtils;
+
+import framework.GLMatrixStack;
+
+
+public class GLHierarchy {
+	private EventLogger logger;
+
+	private Vector3f posBase;
+	private float angBase;
+	
+	private Vector3f posBaseLeft, posBaseRight;
+	private float scaleBaseZ;
+
+	private float angUpperArm, sizeUpperArm;
+
+	private Vector3f posLowerArm;
+	private float angLowerArm, lenLowerArm, widthLowerArm;
+
+	private Vector3f posWrist;
+	private float angWristRoll, angWristPitch, lenWrist, widthWrist;
+
+	private Vector3f posLeftFinger, posRightFinger;
+	private float angFingerOpen, lenFinger, widthFinger, angLowerFinger;
+
+	int theProgram, vao;
+	int modelToCameraMatrixUniform;
+	int indexDataLength;
+
+	public GLHierarchy (EventLogger l, int p, int v, int m, int i) {
+		logger = l;
+		logger.flow("GLHierarchy init.");
+
+		theProgram = p;
+		vao = v;
+		modelToCameraMatrixUniform = m;
+		indexDataLength = i;
+
+		posBase = new Vector3f(3.0f, -5.0f, -40.0f);
+		angBase = -45.0f;
+		posBaseLeft = new Vector3f(2.0f, 0.0f, 0.0f);
+		posBaseRight = new Vector3f(-2.0f, 0.0f, 0.0f);
+		scaleBaseZ = 3.0f;
+		angUpperArm = -78.75f;
+		sizeUpperArm = 9.0f;
+		posLowerArm = new Vector3f(0.0f, 0.0f, 8.0f);
+		angLowerArm = 67.5f;
+		lenLowerArm = 5.0f;
+		widthLowerArm = 1.5f;
+		posWrist = new Vector3f(0.0f, 0.0f, 5.0f);
+		angWristPitch = -11.25f;
+		angWristRoll = 56.25f;
+		lenWrist = 2.0f;
+		widthWrist = 2.0f;
+		posLeftFinger = new Vector3f(1.0f, 0.0f, 1.0f);
+		posRightFinger = new Vector3f(-1.0f, 0.0f, 1.0f);
+		angFingerOpen = 45.0f;
+		lenFinger = 2.0f;
+		widthFinger = 0.5f;
+		angLowerFinger = 45.0f;
+	}
+
+	private float clamp(float value,float min,float max) {
+		if (value < min) return min;
+		if (value > max) return max;
+		return value;
+	}
+
+	private void drawHelper(Matrix4f modelToMatrixCameraMatrix) {
+		FloatBuffer theMatrix = BufferUtils.createFloatBuffer(16);
+		modelToMatrixCameraMatrix.store(theMatrix);
+		theMatrix.flip();
+
+		GL20.glUniformMatrix4(modelToCameraMatrixUniform, false, theMatrix);
+		GL11.glDrawElements(GL11.GL_TRIANGLES, indexDataLength, GL11.GL_UNSIGNED_SHORT, 0);
+	}
+
+	public void draw() {
+		GLMatrixStack modelToCameraStack = new GLMatrixStack(logger);
+
+		GL20.glUseProgram(theProgram);
+		GL30.glBindVertexArray(vao);
+
+		modelToCameraStack.translate(posBase);
+		modelToCameraStack.rotateY(angBase);
+
+		//Left base.
+		modelToCameraStack.push();
+		modelToCameraStack.translate(posBaseLeft);
+		modelToCameraStack.scale(new Vector3f(1.0f, 1.0f, scaleBaseZ));
+		drawHelper(modelToCameraStack.top());
+		modelToCameraStack.pop();
+
+		//Right base.
+		modelToCameraStack.push();
+		modelToCameraStack.translate(posBaseRight);
+		modelToCameraStack.scale(new Vector3f(1.0f, 1.0f, scaleBaseZ));
+		drawHelper(modelToCameraStack.top());
+		modelToCameraStack.pop();
+
+		drawUpperArm(modelToCameraStack);
+
+		GL20.glUseProgram(0);
+		GL30.glBindVertexArray(0);
+	}
+
+	private final static float STANDARD_ANGLE_INCREMENT = 11.25f;
+	private final static float SMALL_ANGLE_INCREMENT = 9.0f;
+
+	public void adjBase (boolean inc) {
+		angBase += (inc? STANDARD_ANGLE_INCREMENT : - STANDARD_ANGLE_INCREMENT);
+		angBase = angBase % 360.0f;
+	}
+
+	public void adjUpperArm (boolean inc) {
+		angUpperArm += (inc? STANDARD_ANGLE_INCREMENT : - STANDARD_ANGLE_INCREMENT);
+		angUpperArm = clamp(angUpperArm, -90.0f, 0.0f);
+	}
+
+	public void adjLowerArm (boolean inc) {
+		angLowerArm += (inc? STANDARD_ANGLE_INCREMENT : - STANDARD_ANGLE_INCREMENT);
+		angLowerArm = clamp(angLowerArm, 0.0f, 146.25f);
+	}
+
+	public void adjWristPitch (boolean inc) {
+		angWristPitch += (inc? STANDARD_ANGLE_INCREMENT : - STANDARD_ANGLE_INCREMENT);
+		angWristPitch = angWristPitch % 360.0f;
+	}
+
+	public void adjWristRoll (boolean inc) {
+		angWristRoll += (inc? STANDARD_ANGLE_INCREMENT : - STANDARD_ANGLE_INCREMENT);
+		angWristRoll = angWristRoll % 360.0f;
+	}
+
+	public void adjFingerOpen (boolean inc) {
+		angFingerOpen += (inc? STANDARD_ANGLE_INCREMENT : - STANDARD_ANGLE_INCREMENT);
+		angFingerOpen = clamp(angFingerOpen, 9.0f, 180.0f);
+	}
+
+	public void writePose() {
+		logger.debug("angBase: " + angBase);
+		logger.debug("angUpperArm: " + angUpperArm);
+		logger.debug("angLowerArm: " + angLowerArm);
+		logger.debug("angWristPitch: " + angWristPitch);
+		logger.debug("angWristRoll: " + angWristRoll);
+		logger.debug("angFingerOpen: " + angFingerOpen);
+		logger.debug("");
+	}
+
+	private void drawFingers (GLMatrixStack modelToCameraStack) {
+		//Left finger.
+		modelToCameraStack.push();
+		modelToCameraStack.translate(posLeftFinger);
+		modelToCameraStack.rotateY(angFingerOpen);
+
+		modelToCameraStack.push();
+		modelToCameraStack.translate(new Vector3f(0.0f, 0.0f, lenFinger / 2.0f));
+		modelToCameraStack.scale(new Vector3f(widthFinger / 2.0f, widthFinger / 2.0f, lenFinger / 2.0f));
+		drawHelper(modelToCameraStack.top());
+		modelToCameraStack.pop();
+
+		//Left lower finger;
+		modelToCameraStack.push();
+		modelToCameraStack.translate(new Vector3f(0.0f, 0.0f, lenFinger));
+		modelToCameraStack.rotateY(-angLowerFinger);
+
+		modelToCameraStack.push();
+		modelToCameraStack.translate(new Vector3f(0.0f, 0.0f, lenFinger / 2.0f));
+		modelToCameraStack.scale(new Vector3f(widthFinger / 2.0f, widthFinger / 2.0f, lenFinger / 2.0f));
+		drawHelper(modelToCameraStack.top());
+		modelToCameraStack.pop();
+
+		modelToCameraStack.pop();
+
+		modelToCameraStack.pop();
+
+		//Right finger.
+		modelToCameraStack.push();
+		modelToCameraStack.translate(posRightFinger);
+		modelToCameraStack.rotateY(-angFingerOpen);
+
+		modelToCameraStack.push();
+		modelToCameraStack.translate(new Vector3f(0.0f, 0.0f, lenFinger / 2.0f));
+		modelToCameraStack.scale(new Vector3f(widthFinger / 2.0f, widthFinger / 2.0f, lenFinger / 2.0f));
+		drawHelper(modelToCameraStack.top());
+		modelToCameraStack.pop();
+
+		//Right lower finger;
+		modelToCameraStack.push();
+		modelToCameraStack.translate(new Vector3f(0.0f, 0.0f, lenFinger));
+		modelToCameraStack.rotateY(angLowerFinger);
+
+		modelToCameraStack.push();
+		modelToCameraStack.translate(new Vector3f(0.0f, 0.0f, lenFinger / 2.0f));
+		modelToCameraStack.scale(new Vector3f(widthFinger / 2.0f, widthFinger / 2.0f, lenFinger / 2.0f));
+		drawHelper(modelToCameraStack.top());
+		modelToCameraStack.pop();
+
+		modelToCameraStack.pop();
+
+		modelToCameraStack.pop();
+	}
+
+	private void drawWrist (GLMatrixStack modelToCameraStack) {
+		modelToCameraStack.push();
+		modelToCameraStack.translate(posWrist);
+		modelToCameraStack.rotateZ(angWristRoll);
+		modelToCameraStack.rotateX(angWristPitch);
+
+		modelToCameraStack.push();
+		modelToCameraStack.scale(new Vector3f(widthWrist / 2.0f, widthWrist / 2.0f, lenWrist / 2.0f));
+		drawHelper(modelToCameraStack.top());
+		modelToCameraStack.pop();
+
+		drawFingers(modelToCameraStack);
+
+		modelToCameraStack.pop();
+	}
+
+	private void drawLowerArm (GLMatrixStack modelToCameraStack) {
+		modelToCameraStack.push();
+		modelToCameraStack.translate(posLowerArm);
+		modelToCameraStack.rotateX(angLowerArm);
+
+		modelToCameraStack.push();
+		modelToCameraStack.translate(new Vector3f(0.0f, 0.0f, lenLowerArm / 2.0f));
+		modelToCameraStack.scale(new Vector3f(widthLowerArm / 2.0f, widthLowerArm / 2.0f, lenLowerArm / 2.0f));
+		drawHelper(modelToCameraStack.top());
+		modelToCameraStack.pop();
+
+		drawWrist(modelToCameraStack);
+
+		modelToCameraStack.pop();
+	}
+
+	private void drawUpperArm (GLMatrixStack modelToCameraStack) {
+		modelToCameraStack.push();
+		modelToCameraStack.rotateX(angUpperArm);
+		
+		modelToCameraStack.push();
+		modelToCameraStack.translate(new Vector3f(0.0f, 0.0f, (sizeUpperArm / 2.0f) - 1.0f));
+		modelToCameraStack.scale(new Vector3f(1.0f, 1.0f, sizeUpperArm / 2.0f));
+		drawHelper(modelToCameraStack.top());
+		modelToCameraStack.pop();
+
+		drawLowerArm(modelToCameraStack);
+
+		modelToCameraStack.pop();
+	}
+}
---
+++ src/GLMatrixStack.java
@@ -0,0 +1,118 @@
+package framework;
+
+//TODO update when new util is out
+import org.lwjgl.util.vector.Vector3f;
+import org.lwjgl.util.vector.Matrix3f;
+import org.lwjgl.util.vector.Matrix4f;
+import java.util.Stack;
+
+public class GLMatrixStack {
+	private EventLogger logger;
+	private Stack<Matrix4f> matrices;
+	private Matrix4f currentMatrix;
+
+	public GLMatrixStack (EventLogger l) {
+		logger = l;
+		logger.flow("GLMatrixStack init");
+
+		matrices = new Stack<Matrix4f>();
+		currentMatrix = new Matrix4f();
+	}   
+
+	public Matrix4f top() { return currentMatrix;}
+
+	private Matrix4f fill4With3(Matrix3f in) {
+		Matrix4f out = new Matrix4f();
+		out.setZero();
+		out.m33 = 1.0f;
+
+		out.m00 = in.m00; out.m01 = in.m01; out.m02 = in.m02;
+		out.m10 = in.m10; out.m11 = in.m11; out.m12 = in.m12;               
+		out.m20 = in.m20; out.m21 = in.m21; out.m22 = in.m22;
+		return out;
+	}   
+
+	private Matrix3f RotateXHelper (float angle) {
+		angle = (float)Math.toRadians(angle);
+		float cos = (float)Math.cos(angle);
+		float sin = (float)Math.sin(angle);
+
+		Matrix3f result = new Matrix3f();
+		result.m11 = cos;
+		result.m12 = sin;
+		result.m21 = -sin;
+		result.m22 = cos;
+
+		return result;
+	}
+
+	private Matrix3f RotateYHelper (float angle) {
+		angle = (float)Math.toRadians(angle);
+		float cos = (float)Math.cos(angle);
+		float sin = (float)Math.sin(angle);
+
+		Matrix3f result = new Matrix3f();
+		result.m00 = cos;
+		result.m02 = -sin;
+		result.m20 = sin;
+		result.m22 = cos;
+
+		return result;
+	}
+
+	private Matrix3f RotateZHelper (float angle) {
+		angle = (float)Math.toRadians(angle);
+		float cos = (float)Math.cos(angle);
+		float sin = (float)Math.sin(angle);
+
+		Matrix3f result = new Matrix3f();
+		result.m00 = cos;
+		result.m01 = sin;
+		result.m10 = -sin;
+		result.m11 = cos; 
+
+		return result;
+	}
+
+	public void rotateX (float angle) {
+		Matrix4f rotationMatrix = fill4With3(RotateXHelper(angle));
+		currentMatrix = Matrix4f.mul(currentMatrix, rotationMatrix, null);
+	}
+
+	public void rotateY (float angle) {
+		Matrix4f rotationMatrix = fill4With3(RotateYHelper(angle));
+		currentMatrix = Matrix4f.mul(currentMatrix, rotationMatrix, null);
+	}
+
+	public void rotateZ (float angle) {
+		Matrix4f rotationMatrix = fill4With3(RotateZHelper(angle));
+		currentMatrix = Matrix4f.mul(currentMatrix, rotationMatrix, null);
+	}
+
+	public void scale (Vector3f scale) {
+		Matrix4f scaleMatrix = new Matrix4f();
+		scaleMatrix.m00 = scale.x;
+		scaleMatrix.m11 = scale.y;
+		scaleMatrix.m22 = scale.z;
+
+		currentMatrix = Matrix4f.mul(currentMatrix, scaleMatrix, null);
+	}
+
+	public void translate (Vector3f offset) {
+		Matrix4f translateMatrix = new Matrix4f();
+		translateMatrix.m30 = offset.x;
+		translateMatrix.m31 = offset.y;
+		translateMatrix.m32 = offset.z;
+		translateMatrix.m33 = 1.0f;
+
+		currentMatrix = Matrix4f.mul(currentMatrix, translateMatrix, null);
+	}
+
+	public void push() {
+		matrices.push(currentMatrix);
+	}
+
+	public void pop() {
+		currentMatrix = matrices.pop();
+	}
+}
